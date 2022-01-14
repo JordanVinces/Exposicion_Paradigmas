1b06cc4e3f96d1ee000a1f4fde918a4c1b7d7314
JordanVinces <94881562+JordanVinces@users.noreply.github.com>
2022-01-14 13:46:13 -0500

    Add files via upload

---
+++ Exposicion_Paradigmas/Exposicion_Paradigmas/Explicacion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exposicion_Paradigmas
+{
+    internal class Explicacion
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int> { 1, 5, 6, 3, 1, 3, 8, 10};
+            
+            //Paradigma Imperativo
+            var max5 = new List<int>();
+
+            foreach(var num in numbers)
+            {
+                if(num > 5)
+                    max5.Add(num*10);
+            }
+
+            //Programacion Funcional y Paradigma Declarativo 
+            var max5D = numbers.Where(d => d > 5).Select(d=>d*10).ToList();
+        }
+
+    }
+}
